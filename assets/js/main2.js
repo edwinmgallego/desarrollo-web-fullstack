48f8cfd9cc2801a2e3d530f29bdcfc10fc0ab029
nicoleandreaa2005@gmail.com <nicoleandreaa2005@gmail.com>
2024-11-10 17:52:58 -0500

    proyecto todo

---
+++ assets/js/main2.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems, {
+      hover: false, 
+      coverTrigger: false
+    });
+  });
+
+  document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.news-cards', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
